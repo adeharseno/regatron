6d7a7db8e094a5fe8077c69ed2fbb9252589e0bc
Ade Harseno <ade.harseno@gmail.com>
2026-07-30 19:39:25 +0700

    gtm implementation

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -14,6 +14,7 @@ import { getSiteSettings } from '@/sanity/lib/site-settings'
 import '../globals.css'
 
 const GOOGLE_ANALYTICS_ID = 'G-RJJQGD8QE0'
+const GOOGLE_TAG_MANAGER_ID = 'GTM-KRT88VZX'
 
 const plusJakarta = Plus_Jakarta_Sans({
   subsets: ['latin'],
@@ -54,7 +55,24 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale satisfies Locale} className={`${plusJakarta.variable} bg-background`}>
+      <Script id="google-tag-manager" strategy="beforeInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${GOOGLE_TAG_MANAGER_ID}');
+        `}
+      </Script>
       <body className="font-sans antialiased">
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GOOGLE_TAG_MANAGER_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Suspense fallback={null}>
           <NavigationProgress />
         </Suspense>
